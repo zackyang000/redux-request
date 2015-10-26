08a9f933628c268e34644e43fd9a3ea6dd61af22
Zack Yang <zackyang@outlook.com>
2015-10-26 17:14:59 +0800

    first commit

---
+++ src/Request.js
@@ -0,0 +1,40 @@
+import superagent from 'superagent';
+
+export default class Request {
+  constructor(apiRoot) {
+    ['get', 'post', 'put', 'patch', 'del'].
+      map((method) => {
+        this[method] = (path, options) => {
+          return new Promise((resolve, reject) => {
+            const request = superagent[method](this.formatUrl(path, apiRoot));
+            if (options && options.query) {
+              request.query(options.query);
+            }
+            if (options && options.data) {
+              request.send(options.data);
+            }
+            request.end((err, res) => {
+              if (err) {
+                reject((res && res.body) || err);
+              } else {
+                resolve(res.body);
+              }
+            });
+          });
+        };
+      });
+  }
+
+  formatUrl(path, apiRoot) {
+    if (hasProtocol(path)) {
+      return path;
+    }
+    path = path[0] !== '/' ? '/' + path : path;
+    return apiRoot + path;
+  }
+
+  hasProtocol(path) {
+    return apiRoot.indexOf('http://') === 0 || apiRoot.indexOf('https://') === 0
+  }
+}
+
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import Request from './request';
+
+export default function requestMiddleware({apiRoot, handle}) {
+  const request = new Request(apiRoot);
+
+  return ({dispatch, getState}) => {
+    return next => action => {
+      if (typeof action === 'function') {
+        return action(dispatch, getState);
+      }
+
+      const { promise, type, ...params } = action;
+
+      if (!promise) {
+        action.readyState = 'success';
+        return next(action);
+      }
+
+      next({...params, type, readyState: 'request'});
+      return promise(request).then((result) => {
+        if (handle) {
+          let msg = handle(result);
+          if (msg === true) {
+            next({...params, result, type, readyState: 'success'});
+          } else {
+            next({...params, error: msg, type, readyState: 'failure'});
+          }
+        } else {
+          next({...params, result, type, readyState: 'success'});
+        }
+      }, (error) => {
+        next({...params, error, type, readyState: 'failure'});
+      }).catch((error)=> {
+        console.error('redux-async-middleware Error: ', error);
+        next({...params, error, type, readyState: 'failure'});
+      });
+    };
+  }
+}
